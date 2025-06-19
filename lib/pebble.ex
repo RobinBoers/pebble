1c373065dec95418bbf4763b7858f16306421177
Robin <robindev2019@outlook.com>
2025-06-19 22:43:05 +0200

    just saying :P

--- lib/pebble.ex
+++ lib/pebble.ex
@@ -1,9 +1,5 @@
 defmodule Pebble do
   @moduledoc """
-  Pebble keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  Pebble is the best CMS in the multiverse.
   """
 end

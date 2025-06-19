c31fd7d8708bb860ac5d0b5040785f35720619f2
Robin <robindev2019@outlook.com>
2025-06-19 22:33:00 +0200

    Minor layout changes

--- lib/pebble_web/live/dashboard_live.ex
+++ lib/pebble_web/live/dashboard_live.ex
@@ -16,14 +16,9 @@ defmodule PebbleWeb.DashboardLive do
     <h3>Quick actions</h3>
 
     <ul>
-      <li><a href="/fragments">Fragments</a></li>
-      <li><a href="/templates">Templates</a></li>
-      <li><a href="/endpoints">Endpoints</a></li>
-      <li><a href="/layouts">Layouts</a></li>
-      <li><a href="/media">Media</a></li>
-      <li><a href="/contacts">Contacts</a></li>
-      <li><a href="/stats">Menu</a></li>
-      <li><a href="/stats">Statistics</a></li>
+      <li><.link navigate={~p"/fragments/new"}>Write anything. Write everything.</.link></li>
+      <li><.link navigate={~p"/media/upload"}>Upload memories</.link></li>
+      <li><.link href="#" target="_blank">Go to geheimesite.nl →</.link></li>
     </ul>
     """
   end

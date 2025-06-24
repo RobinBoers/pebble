d98e95776f1b08587a362ec00af06f848118e801
Robin <robindev2019@outlook.com>
2025-06-25 00:18:49 +0200

    Remove TailwindCSS from `CoreComponents`

--- lib/pebble_web/components/core_components.ex
+++ lib/pebble_web/components/core_components.ex
@@ -163,7 +163,7 @@ defmodule PebbleWeb.CoreComponents do
 
     ~H"""
     <div>
-      <label class="flex items-center gap-4 text-sm leading-6 text-zinc-600">
+      <label>
         <input type="hidden" name={@name} value="false" disabled={@rest[:disabled]} />
         <input
           type="checkbox"
@@ -171,7 +171,6 @@ defmodule PebbleWeb.CoreComponents do
           name={@name}
           value="true"
           checked={@checked}
-          class="rounded border-zinc-300 text-zinc-900 focus:ring-0"
           {@rest}
         />
         {@label}
@@ -188,7 +187,6 @@ defmodule PebbleWeb.CoreComponents do
       <select
         id={@id}
         name={@name}
-        class="mt-2 block w-full rounded-md border border-gray-300 bg-white shadow-sm focus:border-zinc-400 focus:ring-0 sm:text-sm"
         multiple={@multiple}
         {@rest}
       >
@@ -207,11 +205,7 @@ defmodule PebbleWeb.CoreComponents do
       <textarea
         id={@id}
         name={@name}
-        class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6 min-h-[6rem]",
-          @errors == [] && "border-zinc-300 focus:border-zinc-400",
-          @errors != [] && "border-rose-400 focus:border-rose-400"
-        ]}
+        class={@errors != [] && "has-error"}
         {@rest}
       >{Phoenix.HTML.Form.normalize_value("textarea", @value)}</textarea>
       <.error :for={msg <- @errors}>{msg}</.error>
@@ -229,11 +223,7 @@ defmodule PebbleWeb.CoreComponents do
         name={@name}
         id={@id}
         value={Phoenix.HTML.Form.normalize_value(@type, @value)}
-        class={[
-          "mt-2 block w-full rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6",
-          @errors == [] && "border-zinc-300 focus:border-zinc-400",
-          @errors != [] && "border-rose-400 focus:border-rose-400"
-        ]}
+        class={@errors != [] && "has-error"}
         {@rest}
       />
       <.error :for={msg <- @errors}>{msg}</.error>
@@ -249,7 +239,7 @@ defmodule PebbleWeb.CoreComponents do
 
   def label(assigns) do
     ~H"""
-    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+    <label for={@for}>
       {render_slot(@inner_block)}
     </label>
     """
@@ -262,8 +252,8 @@ defmodule PebbleWeb.CoreComponents do
 
   def error(assigns) do
     ~H"""
-    <p class="mt-3 flex gap-3 text-sm leading-6 text-rose-600">
-      <.icon name="hero-exclamation-circle-mini" class="mt-0.5 h-5 w-5 flex-none" />
+    <p>
+      <.icon name="hero-exclamation-circle-mini" />
       {render_slot(@inner_block)}
     </p>
     """

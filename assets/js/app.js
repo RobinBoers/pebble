7b6aaa08b99c68fbe1901d697c385fb9fe17d79b
Robin <robindev2019@outlook.com>
2025-06-25 00:55:59 +0200

    Disable topbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,10 +14,9 @@ let liveSocket = new LiveSocket("/live", Socket, {
   metadata: { keydown: (e, _) => ({ ctrl: e.ctrlKey || e.metaKey }) }
 });
 
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-
-window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
-window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
+// topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+// window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+// window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 liveSocket.connect();
 

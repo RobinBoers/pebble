79ec857de34773714acfe0a6feb003bc4767ad2f
Robin <robindev2019@outlook.com>
2025-06-25 14:58:29 +0200

    Fix bugs in `Ecto.Atom` implementation

--- lib/ecto/atom.ex
+++ lib/ecto/atom.ex
@@ -3,7 +3,14 @@ defmodule Ecto.Atom do
   use Ecto.Type
 
   def type, do: :string
-  def cast(value), do: {:ok, value}
+
+  def cast(value) when is_atom(value), do: {:ok, value}
+  def cast(value) when is_binary(value) do
+    {:ok, String.to_existing_atom(value)}
+  rescue
+    ArgumentError -> {:ok, value}
+  end
+
   def load(value), do: {:ok, String.to_atom(value)}
   def dump(value) when is_atom(value), do: {:ok, Atom.to_string(value)}
   def dump(_), do: :error

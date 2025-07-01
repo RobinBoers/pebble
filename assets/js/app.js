f34cebe7abf0c229a4e171f846f916fb7702ea49
Robin <robindev2019@outlook.com>
2025-07-01 13:05:41 +0200

    Remove unused topbar import

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,8 +2,6 @@ import "phoenix_html";
 import { Socket } from "phoenix";
 import { LiveSocket } from "phoenix_live_view";
 
-import topbar from "../vendor/topbar";
-
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
   .getAttribute("content");

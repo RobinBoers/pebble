c0e0553735585f3effa2a5ef636a0527798b991d
Robin <robindev2019@outlook.com>
2025-08-15 11:06:08 +0200

    `jinja` is now a library

--- mix.exs
+++ mix.exs
@@ -57,8 +57,8 @@ defmodule Pebble.MixProject do
     {:typed_ecto_schema, "~> 0.4.2"},
     {:typedstruct, "~> 0.5.3"},
     {:toml, "~> 0.7"},
-    {:structo, "~> 0.1.4"},
-    {:pythonx, "~> 0.4.5"}
+    {:structo, "~> 0.2.0"},
+    {:jinja, "~> 0.0.1"}
   ]
 
   defp aliases, do: [
